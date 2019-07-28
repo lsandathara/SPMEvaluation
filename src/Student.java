39b530e04df8ddd40b3ea6c4975def360be3a77a
lsandathara <dmlakchani@gmail.com>
2019-07-28 12:46:37 +0530

    student class

---
+++ src/Student.java
@@ -0,0 +1,20 @@
+
+public class Student {
+	private String sId;
+	private String sname;
+	public String getsId() {
+		return sId;
+	}
+	public void setsId(String sId) {
+		this.sId = sId;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+	
+
+}
