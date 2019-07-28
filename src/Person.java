4dc3f5ce5c2d1e4841dff81926f601f733d32b2b
lsandathara <dmlakchani@gmail.com>
2019-07-28 12:43:55 +0530

    first commit

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+
+public class Person {
+	private String pId;
+	private String name;
+	public String getpId() {
+		return pId;
+	}
+	public void setpId(String pId) {
+		this.pId = pId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
